e3898e172b1b4b1a993387100958cd044031e5d9
Pedro <pedro.enrique1599@icloud.com>
2023-03-08 05:28:01 -0300

    search bar element added to restaurant list

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,35 +1,58 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 import { IPaginacao } from '../../interfaces/IPaginacao';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
-import { Button } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 
-export default function ListaRestaurantes () {
+interface IParametrosBusca {
+  ordering?: string
+  search?: string
+}
+
+export default function ListaRestaurantes() {
 
   const buttonPrev = {
     marginTop: 15,
-		textDecoration: 'none',
-		color: "SlateGrey",
-		backgroundColor: "Lavender",
-		fontWeight: "bolder"
-	}
+    textDecoration: 'none',
+    color: "SlateGrey",
+    backgroundColor: "Lavender",
+    fontWeight: "bolder"
+  }
   const buttonNext = {
     marginTop: 15,
     marginLeft: 5,
-		textDecoration: 'none',
-		color: "SlateGrey",
-		backgroundColor: "Lavender",
-		fontWeight: "bolder"
-	}
+    textDecoration: 'none',
+    color: "SlateGrey",
+    backgroundColor: "Lavender",
+    fontWeight: "bolder"
+  }
+  const buttonSearch = {
+    marginLeft: 10,
+    padding: 13,
+    textDecoration: 'none',
+    color: "SlateGrey",
+    backgroundColor: "Lavender",
+    fontWeight: "bolder"
+  }
+  const searchBar = {
+    width:"30%",
+    // color: "SlateGrey",
+    // border: "3px solid SlateGrey",
+    // borderRadius: "4px"
+  }
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
   const [proximaPagina, setProximaPagina] = useState('')
   const [paginaAnterior, setPaginaAnterior] = useState('')
 
-  const carregarDados = (url: string) => {
-    axios.get<IPaginacao<IRestaurante>>(url)
+  const [busca, setBusca] = useState('')
+
+  // agora, o carregarDados recebe opcionalmente as opções de configuração do axios
+  const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
+
+    axios.get<IPaginacao<IRestaurante>>(url, opcoes)
       .then(resposta => {
         setRestaurantes(resposta.data.results)
         setProximaPagina(resposta.data.next)
@@ -40,6 +63,20 @@ export default function ListaRestaurantes () {
       })
   }
 
+  // a cada busca, montamos um objeto de opções
+  const buscar = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+    const opcoes = {
+      params: {
+
+      } as IParametrosBusca
+    }
+    if (busca) {
+      opcoes.params.search = busca
+    }
+    carregarDados('http://localhost:8000/api/v1/restaurantes/', opcoes)
+  }
+
   useEffect(() => {
     // obter restaurantes
     carregarDados('http://localhost:8000/api/v1/restaurantes/')
@@ -47,7 +84,19 @@ export default function ListaRestaurantes () {
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <form onSubmit={buscar}>
+        <TextField
+          id="standard-search"
+          label="Buscar restaurante..."
+          type="search"
+          variant="standard"
+          style={searchBar}
+          onChange={evento => setBusca(evento.target.value)}
+        />
+      <Button style={buttonSearch} variant="contained" disableElevation type='submit'>buscar</Button>
+    </form>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+
     {<Button style={buttonPrev} variant="contained" onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}>
       Página Anterior
     </Button>}
